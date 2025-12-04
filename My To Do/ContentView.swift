2612e8681f390f1484e4a65c8393f973d0792888
Swapnil2656 <swapnilsen2656@gmail.com>
2025-12-04 11:50:52 +0530

    Initial Commit

---
+++ My To Do/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  My To Do
+//
+//  Created by student on 04/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+           
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ My To Do/My_To_DoApp.swift
@@ -0,0 +1,18 @@
+//
+//  My_To_DoApp.swift
+//  My To Do
+//
+//  Created by student on 04/12/25.
+//
+
+import SwiftUI
+//jdhfjdshfjdsuy
+@main
+struct My_To_DoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
